8f65f876f5314b15f0931e5764c70f88b001796d
Grant Beard <Grantxbeard@gmail.com>
2022-02-05 11:25:11 -0700

    Complete book

--- src/book.js
+++ src/book.js
@@ -1,12 +1,44 @@
-function createTitle() {
-  
+function createTitle(bookTitle) {
+  return "The " + bookTitle
 }
+
+function buildMainCharacter(name, age, pronouns) {
+  return {
+  name: name,
+  age: age,
+  pronouns: pronouns,
+  }
+}
+function saveReview(addReview, reviews) {
+  if (reviews.includes(addReview) === false)
+  return reviews.push(addReview)
+}
+
+function calculatePageCount(bookTitle) {
+  return bookTitle.length * 20
+
+}
+
+function writeBook(bookTitle, main, genre) {
+  return {
+  title: bookTitle,
+  mainCharacter: main,
+  pageCount: calculatePageCount(bookTitle),
+  genre: genre,
+  }
+}
+
+function editBook(book) {
+  book.pageCount = book.pageCount * .75
+}
+
+
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
