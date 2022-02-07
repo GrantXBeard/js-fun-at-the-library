e9ee3c61ee179fd5673a5a2f23ecc293e2d0d253
Grant Beard <Grantxbeard@gmail.com>
2022-02-06 19:27:50 -0700

    complete library

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,41 @@
+function createLibrary(name, shelves) {
+return {
+  name: name,
+  shelves: {
+    fantasy: [],
+    fiction: [],
+    nonFiction: [],
+    }
+  }
+}
+
+function addBook(library, book) {
+// find shelf we want to add book //
+// console.log(library.shelves[book.genre])
+// console.log(book.genre)
+// put book on shelf
+var shelfToAddBook = library.shelves[book.genre];
+shelfToAddBook.push(book);
+}
+
+function checkoutBook(title, shelves) {
+  for (var i = 0; i < shelves.length; i++) {
+    if (title === shelves[i].title) {
+      shelves.splice(i,1);
+      return
+    }
+  }
+
+
+
+  // var
+  // var
+  // for
+}
 
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook
 };
